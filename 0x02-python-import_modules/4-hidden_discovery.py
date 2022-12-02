19940760fbfdddc7d5b5f85fbff8390fe70077f0
Esayas188 <110990267+Esayas188@users.noreply.github.com>
2022-12-02 03:51:36 -0500

    correct

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
-    import sys
+if __name__ == "__main__":
     import hidden_4
-    for n in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
